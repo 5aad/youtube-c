e86a2e4a2a70172ec7ef70b3415e2d5540cece31
Saad <saad_anwer@live.com>
2020-08-09 19:51:27 +0500

    init

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB_gvtrRBD7PhCeEDXHCT9Qx_jbE_CotYc",
+    authDomain: "c-fcc6d.firebaseapp.com",
+    databaseURL: "https://c-fcc6d.firebaseio.com",
+    projectId: "c-fcc6d",
+    storageBucket: "c-fcc6d.appspot.com",
+    messagingSenderId: "356038119521",
+    appId: "1:356038119521:web:7471943b22fbefeb5fda20"
+  };
